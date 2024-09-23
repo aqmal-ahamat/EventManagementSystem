fd3a9436a6b64bf0d624a98c9b5af755f7e4f05e
aqmal ahamat <ahamataqmal@gmail.com>
2024-09-23 11:39:37 +0530

    first commit

--- EventManagementSystem/deleteBooking.cs
+++ EventManagementSystem/deleteBooking.cs
@@ -67,6 +67,7 @@ namespace EventManagementSystem
                 }
                 else
                 {
+                    
                     MessageBox.Show($"Event: {evenT} does not exist");
                 }
 
